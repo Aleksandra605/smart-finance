75e205bf7d0a0fba7390f639aacc9ad0289a39da
Aleksandra605 <72212649+Aleksandra605@users.noreply.github.com>
2022-08-23 12:41:02 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { lazy, Suspense, useEffect } from 'react';
 import { Switch } from 'react-router-dom';
-// import { useHistory, useRouteMatch, useLocation } from 'react-router-dom';
+import { useHistory, useRouteMatch, useLocation } from 'react-router-dom';
 import { getCurrentUser } from './redux/auth/auth-operations';
 import { getIsAuthenticated } from './redux/auth/auth-selectors';
 import { connect } from 'react-redux';
